f8d093ed20d5835c30cbc92d1ffbb9cd0e6410bf
Huang Qiang <nickyfoto@gmail.com>
2015-06-25 11:37:04 +0800

    official answer added

--- Problem_Set_1/ps1-3.py
+++ Problem_Set_1/ps1-3.py
@@ -13,6 +13,17 @@ For example, if s = 'abcbcd', then your program should print
 
 Longest substring in alphabetical order is: abc
 '''
+# Official answer
+curString = s[0]
+longest = s[0]
+for i in range(1, len(s)):
+    if s[i] >= curString[-1]:
+        curString += s[i]
+        if len(curString) > len(longest):
+            longest = curString
+    else:
+        curString = s[i]
+print 'Longest substring in alphabetical order is:', longest
 
 # s = 'zyxwvutsrqponmlkjihgfedcba'
 # s = 'azcbobobegghakl'
